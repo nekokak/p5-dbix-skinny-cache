7d5875c0bfb3b747428774fc619a216d1124537a
Atsushi Kobayashi <nekokak@gmail.com>
2010-04-20 17:12:16 +0900

    add しのぷしす

--- lib/DBIx/Skinny/Cache.pm
+++ lib/DBIx/Skinny/Cache.pm
@@ -12,11 +12,34 @@ DBIx::Skinny::Cache -
 
 =head1 SYNOPSIS
 
-  use DBIx::Skinny::Cache;
+    package Mock::Basic;
+    use DBIx::Skinny setup => +{
+        dsn => 'dbi:SQLite:',
+        username => '',
+        password => '',
+        connect_options => { AutoCommit => 1 },
+    };
+    use DBIx::Skinny::Mixin modules => ['Cache::Memory'];
+
+    # in your script:
+    use Mock::Basic;
+    # set data
+    Mock::Basic->memory_cache->param(foo => 'bar');
+    # get data
+    Mock::Basic->memory_cache->param('foo'); # get bar
+    # delete data
+    Mock::Basic->memory_cache->delete('foo');
+    # get_callback
+    Mock::Basic->memory_cache->get_callback(
+        foo => sub {
+            # some process.....
+            'return data for cache';
+        }
+    );
 
 =head1 DESCRIPTION
 
-DBIx::Skinny::Cache is
+DBIx::Skinny::Cache provide cache components.
 
 =head1 AUTHOR
 
